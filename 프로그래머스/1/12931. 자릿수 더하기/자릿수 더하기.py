77d2b21223ee6fa91bfd5e5e9ac4f2ea008117f8
parkjuwon-ai <parkjuwon.seoul@gmail.com>
2025-04-03 18:38:14 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 9.22 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.py
@@ -0,0 +1,15 @@
+def solution(n):
+    add = str(n)
+    lenth=len(add)
+    
+    answer=0
+    
+    for i in range(lenth):
+        answer = answer+ int(add[i])
+
+    
+    
+    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    print('Hello Python')
+
+    return answer
