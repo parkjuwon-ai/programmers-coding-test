1fbeaab11e25e13d4b8f1f7bf06bf60fd448fd91
parkjuwon-ai <parkjuwon.seoul@gmail.com>
2025-08-07 12:20:08 +0900

    [level 2] Title: 숫자의 표현, Time: 5.74 ms, Memory: 9.02 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -0,0 +1,14 @@
+def solution(n):
+    
+    answer = 0
+    a= 1
+    for a in range(1, n+1):
+        sum = 0
+        for i in range (a,n+1):
+            sum += i
+            if sum == n:
+                answer += 1
+                break
+            elif sum > n:
+                break
+    return answer
